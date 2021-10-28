cdeb0e190f12319f423f64973d13b852c22d7fe8
Tripp <clmnbrkr@gmail.com>
2021-10-28 14:54:02 -0400

    Flessed out some ui

--- rps.js
+++ rps.js
@@ -1,23 +1,58 @@
 let weps = ['rock', 'paper', 'scissors'];
+let humWep = '';
+let humPoints = 0;
+let comPoints = 0;
+let tiePoints = 0;
+let games = 0;
+let humScoreboard = document.querySelector('.humPoints');
+let compScoreBoard = document.querySelector('.compPoints');
+let tieScoreBoard = document.querySelector('.tiePoints');
+let messageBoard = document.querySelector('.messageBoard');
+
+function displayPoints(humPoints, comPoints, tiePoints) {
+    let message = `HUMAN: ${humPoints} TIES: ${tiePoints} COMPUTER: ${comPoints}`;
+    humScoreboard.textContent = `HUMAN: ${humPoints}`;
+    compScoreBoard.textContent = `COMP: ${comPoints}`;
+    tieScoreBoard.textContent = `TIES: ${tiePoints}`;
+};
+
+function displayResults(humWeapon, compWeapon, winner) {
+    let result = ``;
+    if (winner === 'tie') {
+        result = `It's a tie!!`;
+    } else if (winner === 'hum') {
+        result = `Human Wins Round!`;
+    } else {
+        result = `Computer Wins Round!`;
+    };
+    let message = `Human selected ${humWeapon} and Computer selected ${compWeapon}!! ${result}!`;
+    messageBoard.textContent = message;
+};
 
 function computerSelection() {
     const ran = Math.floor(Math.random() * weps.length);
     return weps[ran];
 };
-function playRound(comp, hum) {
-    let winner = ""
-    if (comp === hum) {
-        winner = 'tie'
-    } else if (comp === 'rock' & hum === 'scissors' ||
-    comp === 'paper' & hum === 'rock' ||
-    comp === 'scissors' & hum === "paper") {
-        winner = 'comp'
-    } else {winner = 'hum'};
-    console.log(winner);
-    return winner
-};
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(computerSelection(), prompt("Rock paper or scissors: ").toLowerCase());
+
+function getHumWep(e) {
+    const humWeapon = e.target.id;
+    const compWeapon = computerSelection();
+    if (weps.includes(humWeapon)) {
+        if (humWeapon === compWeapon) {
+            tiePoints++;
+            displayResults(humWeapon, compWeapon, 'tie')
+            displayPoints(humPoints, comPoints, tiePoints)
+        } else if (humWeapon === 'rock' && compWeapon === 'scissors' ||
+        humWeapon === 'paper' && compWeapon === 'rock' ||
+        humWeapon === 'scissors' && compWeapon === 'paper') {
+            humPoints++;
+            displayResults(humWeapon, compWeapon, 'hum')
+            displayPoints(humPoints, comPoints, tiePoints);
+        } else {
+            comPoints++;
+            displayResults(humWeapon, compWeapon, 'comp')
+            displayPoints(humPoints, comPoints, tiePoints);
+        };
     };
 };
+window.addEventListener('click', getHumWep);
